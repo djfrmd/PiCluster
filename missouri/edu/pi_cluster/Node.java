1798bb829f91e02cd31fda01fc8962ceb5532c15
Dallas Fletchall <dallas.fletchall@gmail.com>
2014-10-16 20:30:52 -0500

    Restructure Node to create be main

--- missouri/edu/pi_cluster/Node.java
+++ missouri/edu/pi_cluster/Node.java
@@ -1,9 +1,64 @@
 package missouri.edu.pi_cluster;
 
-public abstract class Node{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+//import com.pi4j.system.NetworkInfo;
+
+public class Node{
     
-    protected final int     MASTER_PORT = 2000;
-    protected final int     SLAVE_PORT  = 2001;
-    protected LocalNodeInfo info        = null;
+    public static void main( String[] args ) {
+    	
+        int            MASTER_PORT = 2000;
+        String         MASTER      = "10.100.100.2";
+        Socket         socket      = null;
+        PrintWriter    output      = null;
+        BufferedReader input       = null;
+        
+    	// Temperature Thread Creation
+    	TemperatureThread tThread = new TemperatureThread();
+    	tThread.start();
+    	
+    	while(true){
+    		
+	    	// Create connection with master
+	    	try {
+				socket = new Socket(MASTER, MASTER_PORT);
+				System.out.println("Connected on Port " + MASTER_PORT);
+				
+				output = new PrintWriter(socket.getOutputStream(), true); 
+				input  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			} catch (IOException e) {
+				Log.write(e.getStackTrace());
+				System.exit(1);
+			}
+	    	
+	    	// Handle messages
+	    	String msg = "This should never be printed";
+	    	try {
+	    		
+				while((msg = input.readLine()) != null){
+					
+					System.out.println("inside loop");
+					new MessageThread(msg, output);
+					
+				}
+			} catch (IOException e) {
+				Log.write(e.getStackTrace());
+			}
+	    	
+	    	// Close socket and try to reconnect
+	    	try {
+				socket.close();
+			} catch (IOException e) {
+				Log.write(e.getStackTrace());
+			}
+	    	
+    	}
+    	
+    }
     
 }
