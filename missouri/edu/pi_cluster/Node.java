d19ae1f9496f73c1c72a41f5691cc0d55b45f211
Dallas Fletchall <dallas.fletchall@gmail.com>
2014-11-01 01:06:03 -0500

    Fix temperature reporting thread send packets

--- missouri/edu/pi_cluster/Node.java
+++ missouri/edu/pi_cluster/Node.java
@@ -2,34 +2,38 @@ package missouri.edu.pi_cluster;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.MulticastSocket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Node{
     
     public static void main( String[] args ) {
     	
         int               MASTER_PORT = 2000;
-        MulticastSocket   socket      = null;
+        String            host        = "10.100.100.2";
+        DatagramSocket    socket      = null;
+        InetAddress       address     = null;
         String            msg         = null;
         
-    	// Temperature Thread Creation
-    	TemperatureThread tThread  = new TemperatureThread();
-    	tThread.start();
-    	
     	while(true){
     		
 	    	// Create connection with master
 	    	try {
-				socket = new MulticastSocket(MASTER_PORT);
+				socket  = new DatagramSocket(MASTER_PORT);
+				address = InetAddress.getByName(host);
 				System.out.println("Connected on Port " + MASTER_PORT);
 			} catch (IOException e) {
 				Log.write(e.getStackTrace());
+				e.printStackTrace();
 				break;
 			}
 	    	
+	    	// Temperature Thread Creation
+			new TemperatureThread(socket, address, MASTER_PORT).start();
+	    	
 	    	// Handle messages
     		while(true){
-    			DatagramPacket packet = new DatagramPacket(new byte[256],256);
+    			DatagramPacket packet = new DatagramPacket(new byte[100],100);
     			try {
 					socket.receive(packet);
 				} catch (IOException e) {
--- missouri/edu/pi_cluster/TemperatureThread.java
+++ missouri/edu/pi_cluster/TemperatureThread.java
@@ -1,15 +1,28 @@
 package missouri.edu.pi_cluster;
 
 import java.io.IOException;
-import com.pi4j.system.SystemInfo;;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import com.pi4j.system.SystemInfo;
 
 public class TemperatureThread extends Thread {
 	
-	private final float MAX_TEMP    = 60;
-	private       float temperature = 0;
+	private DatagramSocket socket     = null;
+	private InetAddress    address    = null;
+	private DatagramPacket packet     = null;
+	private String         msg        = null;
+	private       int      port       = 0;
+	private final float    MAX_TEMP   = 60;
+	private       float    temp       = 0;
+	private final int      timeout_ms = 30000;
 	
-	public TemperatureThread(){
-		super();
+	public TemperatureThread(DatagramSocket socket, InetAddress address, int port){
+		super("TempThread");
+		this.socket  = socket;
+		this.address = address;
+		this.port    = port;
 		setDaemon(true);
 	}
 	
@@ -17,17 +30,16 @@ public class TemperatureThread extends Thread {
 	public void run(){
 		while(true){
 			try {
-				temperature = SystemInfo.getCpuTemperature();
-				if(temperature > MAX_TEMP){
-					Log.write("Overtemp @ " + temperature);
+				sleep(timeout_ms);
+				temp   = SystemInfo.getCpuTemperature();
+				msg    = new String("temp:" + temp);
+				packet = new DatagramPacket(msg.getBytes(), msg.getBytes().length, address, port);
+				socket.send(packet);
+				if(temp > MAX_TEMP){
+					Log.write("Overtemp @ " + temp);
 				}
-				sleep(5000);
 
-			} catch (NumberFormatException e) {
-				Log.write(e.getStackTrace());
-			} catch (IOException e) {
-				Log.write(e.getStackTrace());
-			} catch (InterruptedException e) {
+			} catch (NumberFormatException | IOException | InterruptedException e) {
 				Log.write(e.getStackTrace());
 			}
 		}
