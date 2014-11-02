9af2f1ec9b4572435aec85ae13eb8e85979ec73d
Dallas Fletchall <dallas.fletchall@gmail.com>
2014-11-02 15:39:36 -0600

    Add reset driver

--- missouri/edu/pi_cluster/MessageThread.java
+++ missouri/edu/pi_cluster/MessageThread.java
@@ -2,11 +2,13 @@ package missouri.edu.pi_cluster;
 
 import java.io.IOException;
 
+import com.pi4j.io.gpio.PinState;
+
 public class MessageThread extends Thread {
 
     public MessageThread(String msg, MessageList msgList) {
         super("MsgThread");
-        this.message = msg;
+        this.message = msg.trim();
         this.messages = msgList;
     }
 
@@ -17,7 +19,7 @@ public class MessageThread extends Thread {
         String cmd = args[0].trim();
         switch (cmd) {
             case "reset":
-                system("shutdown -r now");
+                reset(args);
                 break;
             case "shutdown":
                 system("shutdown -h now");
@@ -27,6 +29,67 @@ public class MessageThread extends Thread {
                 Log.write("Unknown message: " + cmd);
                 break;
         }
+
+    }
+
+    private void reset(String[] cmd){
+        
+        // Verify message format
+        if(cmd.length < 2){
+            system("shutdown -r now");
+            System.exit(1);
+        }
+        String[] ip = cmd[1].split("\\.");
+        if(ip.length != 4){
+            Log.write("Unknown ip: " + cmd[1]);
+            System.out.println("Unknown ip: " + cmd[1]);
+            return;
+        }
+
+        // Get the corresponding states of the reset pins
+        int[]      vals   = new int[4];
+        PinState[] states = new PinState[4];
+        try{
+            vals[UNIT] = Integer.parseInt(ip[UNIT]);
+            vals[ROW]  = Integer.parseInt(ip[ROW]);
+            vals[COL]  = Integer.parseInt(ip[COL]);
+            
+        }catch(NumberFormatException e){
+            Log.write("Unknown ip: " + cmd[1]);
+            return;
+        }
+
+        // Set corresponding Pin high if matches unit, row, or col
+        states[UNIT] = Node.getUnit() == vals[UNIT] ? HIGH : LOW;
+        states[ROW]  = Node.getRow()  == vals[ROW]  ? HIGH : LOW;
+        states[COL]  = Node.getCol()  == vals[COL]  ? HIGH : LOW;
+
+        // Check for entire unit, row, or col reset
+        states[UNIT] = vals[ROW] == -1 || vals[COL]  == -1 ? HIGH : states[UNIT];
+        states[ROW]  = vals[COL] == -1 || vals[UNIT] == -1 ? HIGH : states[ROW];
+        states[COL]  = vals[ROW] == -1 || vals[UNIT] == -1 ? HIGH : states[COL];
+
+        // If all pins are high, reset this node
+        if( states[UNIT] == HIGH && states[ROW] == HIGH && states[COL] == HIGH){
+            system("shutdown -r now");
+            System.exit(1);
+        }
+
+        // Set pins high for 5 sec
+        GPIO.setPin(Node.RST_UNIT, states[UNIT]);
+        GPIO.setPin(Node.RST_ROW, states[ROW]);
+        GPIO.setPin(Node.RST_COL, states[COL]);
+
+        try {
+            sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Set pins low
+        GPIO.setPin(Node.RST_UNIT, LOW);
+        GPIO.setPin(Node.RST_ROW, LOW);
+        GPIO.setPin(Node.RST_COL, LOW);
 
     }
 
@@ -52,8 +115,13 @@ public class MessageThread extends Thread {
         messages.add(msg);
     }
 
-    private MessageList messages   = null;
-    private String      message    = null;
-    private final int   timeout_ms = 10000;
+    private MessageList    messages   = null;
+    private String         message    = null;
+    private final int      timeout_ms = 10000;
+    private final PinState HIGH       = PinState.HIGH;
+    private final PinState LOW        = PinState.LOW;
+    private final int      UNIT       = 1;
+    private final int      ROW        = 2;
+    private final int      COL        = 3;
 
 }
--- missouri/edu/pi_cluster/Node.java
+++ missouri/edu/pi_cluster/Node.java
@@ -3,36 +3,62 @@ package missouri.edu.pi_cluster;
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import com.pi4j.io.gpio.PinState;
 
 public class Node{
     
     public static void main( String[] args ) {
     	
+        // Set the static UNIT, ROW, & COL
+        if(args.length != 1){
+            System.out.println("Must provide only an ip address");
+            System.exit(1);
+        }
+        String[] ip = args[0].split("\\.");
+        if(ip.length != 4){
+            System.out.println("Unknown ip address " + args[0]);
+            System.out.println("Number of octets = " + ip.length);
+            System.out.println("Number of args = " + args.length);
+            System.exit(1);
+        }
+        try{
+            UNIT = Integer.parseInt(ip[1]);
+            ROW  = Integer.parseInt(ip[2]);
+            COL  = Integer.parseInt(ip[3]);
+        }catch(NumberFormatException e){
+            System.out.println("Unknown ip address " + args[0]);
+            System.exit(1);
+        }
+
+        // Setup GPIO
+        GPIO.setPin(Node.FAN, PinState.LOW);
+
         int            MASTER_PORT = 2000;
         String         host        = "10.100.100.2";
         DatagramSocket socket      = null;
         InetAddress    address     = null;
         MessageList    msgList     = new MessageList();
         final int      timeout_ms  = 10000;
-        
-    	while(true){
-    		
-	    	// Create socket connection with master
+
+        while(true){
+
+            // Create socket connection with master
 	    	try {
 				socket  = new DatagramSocket(MASTER_PORT);
 				address = InetAddress.getByName(host);
 				System.out.println("Connected on Port " + MASTER_PORT);
+				System.out.println("Unit " + UNIT + ", Row " + ROW + ", Column " + COL);
 			} catch (IOException e) {
 				Log.write(e.getStackTrace());
 				System.out.println(e.getMessage());
 				System.exit(1);
 			}
-	    	
+
 	    	// Create helper threads
 			new TemperatureThread(msgList).start();
 			new RxThread(socket, msgList).start();
 			new TxThread(socket, address, MASTER_PORT, msgList).start();
-			
+
 			// Do nothing while the socket is open
     		while(!socket.isClosed()){
     		    try {
@@ -41,14 +67,21 @@ public class Node{
                     e.printStackTrace();
                 }
     		}
-	    	
-    	}
-    	
+
+        }
+
     }
-    
-	public static final int FAN      = 0;
-	public static final int RST_ROW  = 16;
-	public static final int RST_COL  = 15;
-	public static final int RST_UNIT = 27;
-    
+
+    public static int getUnit(){ return UNIT; }
+    public static int getRow(){ return ROW; }
+    public static int getCol(){ return COL; }
+
+    private static       int UNIT     = 0;
+    private static       int ROW      = 0;
+    private static       int COL      = 0;
+	public  static final int FAN      = 0;
+	public  static final int RST_ROW  = 16;
+	public  static final int RST_COL  = 15;
+	public  static final int RST_UNIT = 27;
+
 }
